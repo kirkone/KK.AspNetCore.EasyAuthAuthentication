811d291ef4e50e91e5e57df750b171ef2e97f0c6
Paul Jeschke <Paul-Jeschke@web.de>
2018-09-25 12:44:43 +0200

    try to fix bad code. this is bader :D
    (no body understand this :D)

--- src/KK.AspNetCore.EasyAuthAuthentication/EasyAuthAuthenticationDefaults.cs
+++ src/KK.AspNetCore.EasyAuthAuthentication/EasyAuthAuthenticationDefaults.cs
@@ -1,14 +1,11 @@
 ﻿namespace KK.AspNetCore.EasyAuthAuthentication
 {
-    using System;
-    using Microsoft.AspNetCore.Builder;
-
     public static class EasyAuthAuthenticationDefaults
     {
         /// <summary>
         /// Default values related to Azure EasyAuth authentication handler
         /// </summary>
         public const string AuthenticationScheme = "EasyAuth";
-        public static readonly string DisplayName = "Azure Easy Auth";
+        public const string DisplayName = "Azure Easy Auth";        
     }
 }
--- src/KK.AspNetCore.EasyAuthAuthentication/EasyAuthAuthenticationHandler.cs
+++ src/KK.AspNetCore.EasyAuthAuthentication/EasyAuthAuthenticationHandler.cs
@@ -11,6 +11,7 @@ namespace KK.AspNetCore.EasyAuthAuthentication
     using Microsoft.AspNetCore.Authentication;
     using Microsoft.Extensions.Logging;
     using Microsoft.Extensions.Options;
+    using Newtonsoft.Json;
     using Newtonsoft.Json.Linq;
 
     public class EasyAuthAuthenticationHandler : AuthenticationHandler<EasyAuthAuthenticationOptions>
@@ -29,111 +30,137 @@ namespace KK.AspNetCore.EasyAuthAuthentication
         {
             Logger.LogInformation("starting authentication handler for app service authentication");
 
-            if (this.Context.User == null || this.Context.User.Identity == null || this.Context.User.Identity.IsAuthenticated == false)
+            if (
+                this.Context.User == null ||
+                this.Context.User.Identity == null ||
+                this.Context.User.Identity.IsAuthenticated == false
+            )
             {
-                Logger.LogInformation("identity not found, attempting to fetch from auth endpoint /.auth/me");
-
                 var cookieContainer = new CookieContainer();
-
-                HttpClientHandler handler = new HttpClientHandler()
-                {
-                    CookieContainer = cookieContainer
-                };
-
-                var uriString = $"{Context.Request.Scheme}://{Context.Request.Host}";
-
-                Logger.LogDebug("host uri: {0}", uriString);
-
-                foreach (var c in Context.Request.Cookies)
-                {
-                    cookieContainer.Add(new Uri(uriString), new Cookie(c.Key, c.Value));
-                }
-
-                Logger.LogDebug("found {0} cookies in request", cookieContainer.Count);
-
-                foreach (var cookie in Context.Request.Cookies)
-                {
-                    Logger.LogDebug(cookie.Key);
-                }
-
-                //fetch value from endpoint
-                var request = new HttpRequestMessage(HttpMethod.Get, $"{uriString}/.auth/me");
-
-                foreach (var header in Context.Request.Headers)
-                {
-                    if (header.Key.StartsWith("X-ZUMO-"))
-                    {
-                        request.Headers.Add(header.Key, header.Value[0]);
-                    }
-                }
+                HttpClientHandler handler = createHanlder(ref cookieContainer);
+                HttpRequestMessage httpRequest = CreateAuthRequest(ref cookieContainer);
 
                 JArray payload = null;
-
-                using (HttpClient client = new HttpClient(handler))
+                try
                 {
-                    try
-                    {
-                        var response = await client.SendAsync(request);
-                        if (!response.IsSuccessStatusCode)
-                        {
-                            Logger.LogDebug("auth endpoint was not sucessful. Status code: {0}, reason {1}", response.StatusCode, response.ReasonPhrase);
-                            return AuthenticateResult.Fail("Unable to fetch user information from auth endpoint.");
-                        }
-
-                        var content = await response.Content.ReadAsStringAsync();
-
-                        payload = JArray.Parse(content);
-                    }
-                    catch (Exception ex)
-                    {
-                        Logger.LogError(ex.Message);
-                    }
-                };
-
-                if (payload == null)
+                    payload = await getAuthMe(handler, httpRequest);
+                }
+                catch (Exception ex)
                 {
-                    return AuthenticateResult.Fail("Could not retreive json from /me endpoint.");
+                    return AuthenticateResult.Fail(ex.Message);
                 }
 
-                //build up identity from json...
-                var id = payload[0]["user_id"].Value<string>();
-                var idToken = payload[0]["id_token"].Value<string>();
-                var providerName = payload[0]["provider_name"].Value<string>();
-
-                Logger.LogDebug("payload was fetched from endpoint. id: {0}", id);
-
-                var identity = new GenericIdentity(id);
-
-                Logger.LogInformation("building claims from payload...");
-
-                List<Claim> claims = new List<Claim>();
-                foreach (var claim in payload[0]["user_claims"])
-                {
-                    claims.Add(new Claim(claim["typ"].ToString(), claim["val"].ToString()));
-                }
-
-                Logger.LogInformation("Add claims to new identity");
-
-                identity.AddClaims(claims);
-                identity.AddClaim(new Claim("id_token", idToken));
-                identity.AddClaim(new Claim("provider_name", providerName));
-
-                ClaimsPrincipal p = new GenericPrincipal(identity, null); //todo add roles?
-
-                var ticket = new AuthenticationTicket(
-                    p,
-                    EasyAuthAuthenticationDefaults.AuthenticationScheme
-                );
+                //build up identity from json...                
+                AuthenticationTicket ticket = BuildIdentityFromJsonPayload(payload);                
 
                 Logger.LogInformation("Set identity to user context object.");
-                this.Context.User = p;
+                this.Context.User = ticket.Principal;
 
                 Logger.LogInformation("identity build was a success, returning ticket");
                 return AuthenticateResult.Success(ticket);
             }
+            else
+            {
+                Logger.LogInformation("identity already set, skipping middleware");
+                return AuthenticateResult.NoResult();
+            }            
+        }
 
-            Logger.LogInformation("identity already set, skipping middleware");
-            return AuthenticateResult.NoResult();
+        private AuthenticationTicket BuildIdentityFromJsonPayload(JArray payload)
+        {
+            var id = payload[0]["user_id"].Value<string>();
+            var idToken = payload[0]["id_token"].Value<string>();
+            var providerName = payload[0]["provider_name"].Value<string>();
+
+            Logger.LogDebug("payload was fetched from endpoint. id: {0}", id);
+
+            var identity = new GenericIdentity(id);
+
+            Logger.LogInformation("building claims from payload...");
+
+            List<Claim> claims = new List<Claim>();
+            foreach (var claim in payload[0]["user_claims"])
+            {
+                claims.Add(new Claim(claim["typ"].ToString(), claim["val"].ToString()));
+            }
+
+            Logger.LogInformation("Add claims to new identity");
+
+            identity.AddClaims(claims);
+            identity.AddClaim(new Claim("id_token", idToken));
+            identity.AddClaim(new Claim("provider_name", providerName));
+            var p = new GenericPrincipal(identity, null);
+            return new AuthenticationTicket(
+                p,
+                EasyAuthAuthenticationDefaults.AuthenticationScheme
+            );
+        }
+
+        private HttpRequestMessage CreateAuthRequest(ref CookieContainer cookieContainer)
+        {
+            Logger.LogInformation("identity not found, attempting to fetch from auth endpoint /.auth/me");
+
+            var uriString = $"{Context.Request.Scheme}://{Context.Request.Host}";
+
+            Logger.LogDebug("host uri: {0}", uriString);
+
+            foreach (var c in Context.Request.Cookies)
+            {
+                cookieContainer.Add(new Uri(uriString), new Cookie(c.Key, c.Value));
+            }
+
+            Logger.LogDebug("found {0} cookies in request", cookieContainer.Count);
+
+            foreach (var cookie in Context.Request.Cookies)
+            {
+                Logger.LogDebug(cookie.Key);
+            }
+
+            //fetch value from endpoint
+            var request = new HttpRequestMessage(HttpMethod.Get, $"{uriString}/.auth/me");
+            foreach (var header in Context.Request.Headers)
+            {
+                if (header.Key.StartsWith("X-ZUMO-"))
+                {
+                    request.Headers.Add(header.Key, header.Value[0]);
+                }
+            }
+            return request;
+        }
+
+        private static HttpClientHandler createHanlder(ref CookieContainer container)
+        {
+            var handler = new HttpClientHandler()
+            {
+                CookieContainer = container
+            };
+            return handler;
+        }
+
+        private async Task<JArray> getAuthMe(HttpClientHandler handler, HttpRequestMessage httpRequest)
+        {
+            JArray payload = null;
+            using (HttpClient client = new HttpClient(handler))
+            {
+                var response = await client.SendAsync(httpRequest);
+                if (!response.IsSuccessStatusCode)
+                {
+                    Logger.LogDebug("auth endpoint was not sucessful. Status code: {0}, reason {1}", response.StatusCode, response.ReasonPhrase);
+                    throw new WebException("Unable to fetch user information from auth endpoint.");
+                }
+
+                var content = await response.Content.ReadAsStringAsync();
+                try
+                {
+                    payload = JArray.Parse(content);
+                }
+                catch (Exception)
+                {
+                    throw new JsonSerializationException("Could not retreive json from /me endpoint.");
+                }
+
+            };
+            return payload;
         }
     }
 }
