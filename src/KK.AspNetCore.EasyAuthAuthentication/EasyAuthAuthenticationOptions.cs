42d34a640f801da985c027562746d82372070c5c
Kirsten Kluge <privat@kirkone.de>
2018-10-10 10:59:37 +0200

    Add StyleCop (#6)
    - Add StyleCop to the Nuget project
    - Add minimalö rule set
    - Update code to match the rules

--- src/KK.AspNetCore.EasyAuthAuthentication/EasyAuthAuthenticationOptions.cs
+++ src/KK.AspNetCore.EasyAuthAuthentication/EasyAuthAuthenticationOptions.cs
@@ -3,8 +3,15 @@
     using System;
     using Microsoft.AspNetCore.Authentication;
 
+    /// <summary>
+    /// Options for the <see cref="EasyAuthAuthenticationHandler"/>.
+    /// </summary>
     public class EasyAuthAuthenticationOptions : AuthenticationSchemeOptions
     {
+        /// <summary>
+        /// The endpoint where to look for the <c>JSON</c> with the authentification information.
+        /// </summary>
+        /// <value>A relative path to the <c>wwwroot</c> folder.</value>
         public string AuthEndpoint { get; set; } = ".auth/me";
     }
 }
