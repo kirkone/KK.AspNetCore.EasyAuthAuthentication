525e94770a00cb05b56127447a72f7615ca8a493
paule96 <Paul-Jeschke@outlook.com>
2019-06-25 20:26:56 +0200

    check also the Authorization for content

--- src/KK.AspNetCore.EasyAuthAuthentication/Services/EasyAuthWithHeaderService.cs
+++ src/KK.AspNetCore.EasyAuthAuthentication/Services/EasyAuthWithHeaderService.cs
@@ -1,6 +1,7 @@
 namespace KK.AspNetCore.EasyAuthAuthentication.Services
 {
     using System;
+    using System.Linq;
     using System.Text;
     using Microsoft.AspNetCore.Authentication;
     using Microsoft.AspNetCore.Http;
@@ -9,6 +10,7 @@ namespace KK.AspNetCore.EasyAuthAuthentication.Services
 
     internal class EasyAuthWithHeaderService
     {
+        private const string AuthorizationHeader = "Authorization";
         private const string PrincipalNameHeader = "X-MS-CLIENT-PRINCIPAL-NAME";
         private const string PrincipalObjectHeader = "X-MS-CLIENT-PRINCIPAL";
         private const string PrincipalIdpHeaderName = "X-MS-CLIENT-PRINCIPAL-IDP";
@@ -52,16 +54,23 @@ namespace KK.AspNetCore.EasyAuthAuthentication.Services
         {
             var providerName = this.Headers[PrincipalIdpHeaderName][0];
             this.Logger.LogDebug($"payload was fetched from easyauth me json, provider: {providerName}");
+            string headerContent;
+            if (this.Headers.ContainsKey("Authorization"))
+            {
+                headerContent = this.Headers[AuthorizationHeader].FirstOrDefault();
+            }
+            else
+            {
+                headerContent = this.Headers[PrincipalObjectHeader][0];
+            }
 
             this.Logger.LogInformation("building claims from payload...");
             var xMsClientPrincipal = JObject.Parse(
                         Encoding.UTF8.GetString(
-                            Convert.FromBase64String(this.Headers[PrincipalObjectHeader][0])
+                            Convert.FromBase64String(headerContent)
                         )
                     );
-
             var claims = xMsClientPrincipal["claims"].Children<JObject>();
-
             return AuthenticationTicketBuilder.Build(claims, providerName, this.Options);
         }
     }
